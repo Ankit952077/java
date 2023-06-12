c36f0d37543b58de2a595e05d7dd6121b197fb8d
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-06-12 06:43:24 -0400

    Table print

---
+++ Table.java
@@ -0,0 +1,8 @@
+public class Table {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i = 0; i <=10; i++) {
+            System.out.printf("%d X %d =%d\n",n,i,n*i);
+        }
+    }
+}
