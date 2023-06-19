f4595f3dd4c9ae09535b61304acde3ff674c8705
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-06-19 09:36:20 -0400

    Employee  || phone info || area of square || game

--- Opps.java
+++ Opps.java
@@ -1,21 +1,88 @@
-class Employee  {
-    int id;
-    String name;
+import javax.sound.midi.Soundbank;
 
-    public void printDeatails() {
-        System.out.println("My id is" +id);
-        System.out.println("My Name is" +name);
+class Employee{
+    int salary;
+    String name;
+     public int getsalary() {
+        return salary;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String n) {
+         name =n;
+
+ 
     }
 }
-public class Opps{
+class Callphone{
+  public void Call(){
+    System.out.println("Call Friend.......");
+
+  }
+  public void viberate() {
+    System.out.println("Vibeerate........");
+
+  }
+  public void ring() {
+    System.out.println("ringing............");
+  }
+
+}
+   class Squre{
+    int side;
+    public int area(){
+        return side*side;
+    }
+    public int parameter() {
+        return 4*side;
+    }
+   }
+   class Tommy{
+     public void hit() {
+        System.out.println("hitting the enemy....");
+
+     }
+     public void run() {
+        System.out.println("running from the enemy");
+     }
+     public void fire() {
+        System.out.println("firing on the enemy");
+     }
+   }
+    public class Opps {
     public static void main(String[] args) {
-        Employee harry=new Employee();
-        Employee Ankit=new Employee();
-        harry.id=10;
-        harry.name="CodeWithHarry";
-        Ankit.id=11;
-        Ankit.name="Danav";
-        harry.printDeatails();
-        Ankit.printDeatails();
+        
+    Employee ankit=new Employee();
+    ankit.salary=122;
+    ankit.setname("danavji");
+
+    System.out.println(ankit.getsalary());
+    System.out.println(ankit.getname());
+    System.out.println("\nn ");
+
+
+     
+    Callphone asus=new Callphone();
+       
+       asus.viberate();
+       asus.ring();
+       asus.Call();
+       System.out.println("\n");
+    
+     Squre sq=new Squre(); 
+      sq.side =3;
+     System.out.println(sq.area());
+     System.out.println(sq.parameter());
+     System.out.println("\n");
+      
+     Tommy player=new Tommy();
+     player.hit();
+     player.run();
+     player.fire();
+    
+    
+    
+    
     }
-}
+    }
