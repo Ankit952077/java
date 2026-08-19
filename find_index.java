f4ead6adc0630e539b52b3b4bff919ca61d08f8e
ankit kumar <116968419+ankitEn@users.noreply.github.com>
2026-08-19 23:48:12 +0530

    Add files via upload

---
+++ find_index.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class find_index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            }
+            System.out.println("Enter the element to be searched: ");
+            int x = sc.nextInt();
+            int index = -1;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == x) {
+                    index = i;
+                    break;
+                    }
+                    }
+                    if (index == -1) {
+                        System.out.println("Element not found");
+                        }
+                        else {
+                            System.out.println("Element found at index " + index);
+                    
+                                sc.close();
+
+
+    }}
+ }
