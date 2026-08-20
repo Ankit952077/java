faf19cfbb0bb62c44c8c29c6dd013375f298be5a
ankit kumar <116968419+ankitEn@users.noreply.github.com>
2026-08-20 21:09:41 +0530

    Add files via upload

---
+++ frist.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.HashMap;
+
+public class frist {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("mahmoodpur","hathras");
+        map.put("rampur","kasganj");
+        map.put("raynagar","etha");
+        map.put("raya","mathura");
+        map.clear();
+                
+        System.out.println(map);
+    }
+}
